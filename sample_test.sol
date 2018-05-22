aaa73e12a9d1e6fb00ad4d7de6af834eb7b893ac
Elliot Briant <elliotbriant@Elliots-MacBook-Pro.local>
2018-05-22 11:22:47 -0700

    prime data function

--- sample_test.sol
+++ sample_test.sol
@@ -1,3 +1,29 @@
 import "truffle/Assert.sol";
 
-//Proxy contract for testing throws 
+// In Solidity, when performing a contract call that produces an error,
+// Solidity automatically rethrows, meaning it will bubble that error up
+// to the caller. However, with a raw call, the behavior is different: one
+// can catch the error and decide what to do from there on out. If it throws
+// (fails), it will return false. If it succeeds, it will return true.
+
+
+//Proxy contract for testing throws
+contract ThrowProxy {
+  address public target;
+  bytes data;
+
+  function ThrowProxy(address _target) {
+    target = _target;
+  }
+
+  //prime the data using the fallback function
+  function() {
+    data = msg.data;
+  }
+
+  function execute() returns (bool){
+    return target.call(data);
+  }
+}
+
+//
